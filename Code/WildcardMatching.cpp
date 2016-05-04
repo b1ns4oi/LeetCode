e06e60a52cb40cc0af5bb082f59cd6db8a875416
binisforbinary <binisforbinary@gmail.com>
2016-05-04 16:04:01 +0800

    #44. Wildcard Matching

--- Code/WildcardMatching.cpp
+++ Code/WildcardMatching.cpp
@@ -1,110 +1,87 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <cstring>
 
-using std::vector;
-using std::cout;
-using std::endl;
-using std::cin;
-using std::string;
+using namespace std;
 
 class Solution {
 public:
-    bool isMatch(const char *s, const char *p)
-    {
-    	char *newp = filter_p(p);
-        bool ret = helper(s, 0, newp, 0);
-    	delete[] newp;
-    	return ret;
-    }
-    
-    // p[] contains only '*' in range [from...)
-    bool onlystar(const char *p, size_t from)
-    {
-    	for(; p[from] != '\0'; ++from)
-    		if(p[from] != '*')
-    			return false;
-    	return true;
+    // will get "Time Limit Exceeded"
+    bool isMatch(string s, string p) {
+        string p2 = skip_consecutive_star(p);
+        return isMatch_recur(s, s.size()-1, p2, p2.size()-1);
     }
 
-    //
-    char *filter_p(const char *p) 
-    {
-    	vector<char> vec;
-    	for(; *p; ++p) {
-    		if(*p == '*') {
-    			if(*(p+1) != '*')
-    				vec.push_back(*p);
-    		}	
-    		else 
-    			vec.push_back(*p);
-    	}
-    	vec.push_back('\0');
-    	char *newp = new char[vec.size()];
-    	for(int i = 0; i < vec.size(); ++i) {
-    		newp[i] = vec[i];
-    	}
-    	return newp;
+    // Pass
+    bool isMatch_DP(string s, string p) {
+        p = skip_consecutive_star(p);
+        int m = s.size(), n = p.size();
+
+        vector<vector<bool> > v(m+1, vector<bool>(n+1, false));
+        v[0][0] = true;
+        int i, j;
+
+        for (j = 1; j <= n; ++j) {
+            if (p[j-1] == '*') {
+                v[0][j] = v[0][j-1];
+            } 
+        }
+        for (i = 1; i <= m; ++i) {
+            for (j = 1; j <= n; ++j) {
+                if (p[j-1] == '*') {
+                    v[i][j] = v[i-1][j] || v[i][j-1];
+                }
+                else if (p[j-1] == '?' || p[j-1] == s[i-1])
+                    v[i][j] = v[i-1][j-1];
+                else
+                    ;
+            }
+        }
+        return v[m][n];
     }
-    
-    bool helper(const char *s, int indexs, const char *p, int indexp)
-    {
-    	if(indexs == strlen(s) && indexp == strlen(p))
-    		return true;
-    	if(indexs != strlen(s) && indexp == strlen(p))
-    		return false;
-        if(onlystar(p, indexp))
-        	return true;
-    	if(indexp != strlen(p) && indexs == strlen(s))
-    		return false;
-        
-    	if(p[indexp] == '?' )
-    		return helper(s, indexs+1, p, indexp+1);
-    	else if(p[indexp] == '*') {
-    		for(int i = 0; indexs+i <= strlen(s); ++i) {
-    			if(helper(s, indexs+i, p, indexp+1))
-                    return true;
-    		}
+
+private:
+    string skip_consecutive_star(const string& p) {
+        string p2;
+        char prev_char = 0;
+        for (int i = 0; i < p.size(); ++i) {
+            if (prev_char == '*' && p[i] == '*')
+                continue;
+            p2 += p[i];
+            prev_char = p[i];
+        }
+        return p2;
+    }
+    bool isMatch_recur(string &s, int i, string &p, int j) {
+        if (j < 0) {
+            return i < 0;
+        }
+
+        if (p[j] == '*') {
+            // if (i >= 0) {
+            //     return isMatch_recur(s, i-1, p, j) || isMatch_recur(s, i, p, j-1);
+            // }
+            // else
+            //     return isMatch_recur(s, i, p, j-1);
+
+            // Above if-else block can be simplified as:
+            return isMatch_recur(s, i, p, j-1) || (i >= 0 && isMatch_recur(s, i-1, p, j));
+        }
+        else if (i >= 0 && (p[j] == '?' || p[j] == s[i])) {
+            return isMatch_recur(s, i-1, p, j-1);
+        }
+        else
             return false;
-    	}
-    	else
-    		return s[indexs] == p[indexp] ? helper(s, indexs+1, p, indexp+1) : false;
     }
 };
 
-int main()
-{
-	
-	const char *s = "aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba";
-    const char *p = "a*******b";
-    
+
+int main() {
     Solution sol;
-    
-    cout << (sol.isMatch(s, p) ? "true" : "false") << endl;
-    
+    string s = "aa";
+    string p = "*";
+    bool result = sol.isMatch_DP(s, p);
+    if (result)
+      cout << "True" << endl;
+    else
+      cout << "False" <<endl;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
