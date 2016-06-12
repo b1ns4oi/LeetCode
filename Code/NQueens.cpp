c329d697b37f18fe9bccd0f35dff0cef81cb8557
binisforbinary <binisforbinary@gmail.com>
2016-06-13 01:10:33 +0800

    #51. provide another solution to N-Queens

--- Code/NQueens.cpp
+++ Code/NQueens.cpp
@@ -16,7 +16,43 @@ public:
         return res;
     }
 
+    // trading time with space
+    vector<vector<string>> solveNQueens_2(int n) {
+        vector<vector<string> > res;
+        vector<string> s(n, string(n, '.'));
+
+        // vector<bool> flag_col(n, false);
+        // vector<bool> flag_45(2*n-1, false);
+        // vector<bool> flag_135(2*n-1, false);
+        // solveNQueens_recur2(res, s, 0, n, flag_col, flag_45, flag_135);
+
+        vector<bool> flag(5*n-2, false);
+        solveNQueens_recur2(res, s, 0, n, flag);
+
+        return res;
+    }
+
 private:
+    void solveNQueens_recur2(vector<vector<string> >& res, vector<string>& s, int row, int n,
+        vector<bool>& flag) {
+
+        if (row == n) {
+            res.push_back(s);
+            return;
+        }
+
+        for (int col = 0; col < n; ++col) {
+            if (!flag[col] && !flag[n+row+col] && !flag[n+2*n-1 + row-col+n-1]) {
+                s[row][col] = 'Q';
+                flag[col] = flag[n+row+col] = flag[n+2*n-1 + row-col+n-1] = true;
+                solveNQueens_recur2(res, s, row+1, n, flag);
+                flag[col] = flag[n+row+col] = flag[n+2*n-1 + row-col+n-1] = false;
+                s[row][col] = '.';
+            }
+        }
+
+    }
+
     void solveNQueens_recur(vector<vector<string> >& res, vector<string>& s, int row, int n) {
         if (row == n) {
             res.push_back(s);
@@ -59,8 +95,8 @@ private:
 
 int main() 
 {
-	Solution sol;
-    vector<vector<string>> res = sol.solveNQueens(8);
+    Solution sol;
+    vector<vector<string>> res = sol.solveNQueens_2(8);
     
     // for (int i = 0; i < res.size(); ++i) {
     //     for (int j = 0; j < res[i].size(); ++j) {
@@ -68,5 +104,5 @@ int main()
     //     }
     //     cout << " --------------------- " << endl;
     // }
-	cout << res.size() << endl;
+    cout << res.size() << endl;
 }
